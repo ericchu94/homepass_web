facf7da2f8361e7dceca8a7549c4555721ea7f53
Eric Chu <eric@ericchu.net>
2016-03-08 22:11:30 +0000

    Fix cli hostapd

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const execFile = require('mz/child_process').execFile;
 const fs = require('mz/fs');
 
 const hostapdCli = '/usr/bin/hostapd_cli';
-const hostapdCliArgs = ['sta_all'];
+const hostapdCliArgs = ['all_sta'];
 const hostapdMock = 'hostapd_mock.txt';
 
 function getHostapdStatusCli() {
   return execFile(hostapdCli, hostapdCliArgs).then(result => {
-    const stdout = result.stdout;
-    return stdout;
+    return result[0];
   });
 }
 
