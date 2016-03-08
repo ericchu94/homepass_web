64711f5a32f7a1829942cf932582db83bb67b89c
Eric Chu <eric@ericchu.net>
2016-03-08 17:12:57 -0500

    Remove debug logging

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function getHostapdStatusMock() {
 
 function getHostapdStatus() {
   return getHostapdStatusMock().catch(err => {
-    console.log(err);
     throw new Error('Failed to get hostapd status');
   });
 }
